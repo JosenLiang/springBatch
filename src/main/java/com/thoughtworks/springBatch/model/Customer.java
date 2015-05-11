011a1de27cdda540a703d90a8e3b74707345bf94
liazhang <304957127@qq.com>
2015-05-11 22:35:55 +0800

    [Zhang Liang] add job for flat file

---
+++ src/main/java/com/thoughtworks/springBatch/model/Customer.java
@@ -0,0 +1,67 @@
+package com.thoughtworks.springBatch.model;
+
+public class Customer {
+    private int id;
+    private String firstName;
+    private char middleName;
+    private String lastName;
+    private String addressNumber;
+    private String street;
+    private String city;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public char getMiddleName() {
+        return middleName;
+    }
+
+    public void setMiddleName(char middleName) {
+        this.middleName = middleName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getAddressNumber() {
+        return addressNumber;
+    }
+
+    public void setAddressNumber(String addressNumber) {
+        this.addressNumber = addressNumber;
+    }
+
+    public String getStreet() {
+        return street;
+    }
+
+    public void setStreet(String street) {
+        this.street = street;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+}
